772504495dd208b9f5a4b93971922da72bf5197d
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-17 16:42:27 -0500

    Second Try

--- format_output/average_score_2.py
+++ format_output/average_score_2.py
@@ -1,16 +1,16 @@
 """
 Program: average_score_2.py
 Author: Kelly Smith
-Last date updated: 09/09/2019
+Last date updated: 09/17/2019
 
 Program to find the average of three numbers
 """
 
 
-def average(scores):
-    score_1 = input("enter first score")
-    score_2 = input("enter second score")
-    score_3 = input("enter third score")
+def average():
+    score_1 = input("enter first score ")
+    score_2 = input("enter second score ")
+    score_3 = input("enter third score ")
     x = int(score_1)
     y = int(score_2)
     z = int(score_3)
@@ -18,8 +18,8 @@ def average(scores):
     return sum(scores) / len(scores)
 
 
-def scores(args):
-    pass
-
-
-print(average(scores))
+if __name__ == '__main__':
+    first_name = input("Please enter first name ")
+    last_name = input("Please enter last name ")
+    age = input("Please enter age ")
+    print(last_name + ", " + first_name + " " + "Age: " + age + " " + "Average Score: %.2f", (average()))
